42615541076eea400347935ae63a617917c1548b
Kolodkin.SS <zorokon@yandex.ru>
2025-12-13 17:39:12 +0300

    save

---
+++ LinkedLists/141_LinkedListCycle.py
@@ -0,0 +1,40 @@
+"""
+    Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+    There is a cycle in a linked list if there is some node in the list that can be reached
+    again by continuously following the next pointer. Internally, pos is used to denote the
+     index of the node that tail's next pointer is connected to.
+     Note that pos is not passed as a parameter.
+
+    Return true if there is a cycle in the linked list. Otherwise, return false.
+
+    Easy
+"""
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head is None:
+            return False
+
+        list_nodes = set()
+
+        current = head
+        while current is not None:
+            if current in list_nodes:
+                return True
+            else:
+                list_nodes.add(current)
+                current = current.next
+
+        return False
+
+
+# 64,79%
