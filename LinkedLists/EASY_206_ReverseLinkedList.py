abd93695dcf557406b48d3f26fcb2f8f23b5e5f3
Kolodkin.SS <zorokon@yandex.ru>
2025-12-16 10:15:48 +0300

    save

--- LinkedLists/EASY_206_ReverseLinkedList.py
+++ LinkedLists/EASY_206_ReverseLinkedList.py
@@ -22,13 +22,3 @@ class Solution:
             current_node = tmp
 
         return prev.next
-
-        # dummy = ListNode(0)
-        # cur = head
-        # while cur:
-        #     tmp = cur.next
-        #     cur.next = dummy.next
-        #     dummy.next = cur
-        #     cur = tmp
-
-        # return dummy.next
