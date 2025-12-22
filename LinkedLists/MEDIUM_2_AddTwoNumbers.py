381940c25ee7ef62627dc7b644e258ed7c831db5
Kolodkin.SS <zorokon@yandex.ru>
2025-12-22 19:44:06 +0300

    save

--- LinkedLists/MEDIUM_2_AddTwoNumbers.py
+++ LinkedLists/MEDIUM_2_AddTwoNumbers.py
@@ -17,38 +17,23 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        save_value = 0
-        save_node = None
-        head_new_list = None
+        dummy_head = ListNode()
+        current = dummy_head
+        carry = 0
 
-        while l1 and l2:
-            value = l1.val + l2.val + save_value
-            save_value = value // 10
-            value %= 10
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
 
-            node = ListNode(val=value)
-            if save_node:
-                save_node.next = node
-            save_node = node
-            if head_new_list is None:
-                head_new_list = node
+            total = val1 + val2 + carry
+            carry = total // 10
+            node = ListNode(val=total % 10)
+            current.next = node
+            current = node
 
-            l1, l2 = l1.next, l2.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
 
-        l3 = l1 or l2
-        while l3:
-            value = l3.val + save_value
-            save_value = value // 10
-            value %= 10
-
-            node = ListNode(val=value)
-            save_node.next = node
-            save_node = node
-
-            l3 = l3.next
-
-        if save_value:
-            node = ListNode(val=save_value)
-            save_node.next = node
-
-        return head_new_list
+        return dummy_head.next
