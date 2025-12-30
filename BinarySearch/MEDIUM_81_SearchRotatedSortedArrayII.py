ba03f9ff54f91af6150840821eda2c3cec3fa796
Kolodkin.SS <zorokon@yandex.ru>
2025-12-30 15:51:45 +0300

    save

---
+++ BinarySearch/MEDIUM_81_SearchRotatedSortedArrayII.py
@@ -0,0 +1,37 @@
+"""
+    There is an integer array nums sorted in non-decreasing order
+    (not necessarily with distinct values).
+
+    Before being passed to your function, nums is rotated at an unknown
+    pivot index k (0 <= k < nums.length) such that the resulting array
+    is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+    For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5
+    and become [4,5,6,6,7,0,1,2,4,4].
+
+    Given the array nums after the rotation and an integer target,
+    return true if target is in nums, or false if it is not in nums.
+
+    You must decrease the overall operation steps as much as possible.
+"""
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return True
+            if nums[mid] > nums[left]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
+
+
+if __name__ == "__main__":
+    assert Solution().search([1, 0, 1, 1, 1], 0) is True
