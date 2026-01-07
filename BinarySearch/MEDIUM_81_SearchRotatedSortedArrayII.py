5086dba2bd35618a4a5a9be950d7ae275d87438a
Kolodkin.SS <zorokon@yandex.ru>
2026-01-07 13:31:33 +0300

    save

--- BinarySearch/MEDIUM_81_SearchRotatedSortedArrayII.py
+++ BinarySearch/MEDIUM_81_SearchRotatedSortedArrayII.py
@@ -18,20 +18,32 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        left, right = 0, len(nums) - 1
+        left = 0
+        right = len(nums) - 1
 
         while left <= right:
             mid = (left + right) // 2
 
             if nums[mid] == target:
                 return True
-            if nums[mid] > nums[left]:
-                left = mid + 1
+
+            if nums[left] == nums[mid] == nums[right]:
+                left += 1
+                right -= 1
             else:
-                right = mid - 1
+                if nums[left] <= nums[mid]:
+                    if nums[left] <= target < nums[mid]:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+                elif nums[mid] <= nums[right]:
+                    if nums[mid] < target <= nums[right]:
+                        left = mid + 1
+                    else:
+                        right = mid - 1
 
         return False
 
 
 if __name__ == "__main__":
-    assert Solution().search([1, 0, 1, 1, 1], 0) is True
+    assert Solution().search([2, 5, 6, 0, 0, 1, 2], 3) is False
