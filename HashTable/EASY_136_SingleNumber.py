3e883534e45681c53486e11602e0fffd0c0be17c
Kolodkin.SS <zorokon@yandex.ru>
2026-01-10 11:11:12 +0300

    save

---
+++ HashTable/EASY_136_SingleNumber.py
@@ -0,0 +1,17 @@
+"""
+    Given a non-empty array of integers nums, every element appears twice except for one.
+    Find that single one.
+
+    You must implement a solution with a linear runtime complexity and use
+    only constant extra space.
+"""
+
+
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        result = 0
+
+        for num in nums:
+            result ^= num
+
+        return result
